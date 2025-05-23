698bc6c1795ee182729c849fbad58fafede152f6
Lenix <149199196+lenix-x@users.noreply.github.com>
2025-05-23 16:35:01 +0100

    Update function.lua

--- client/function.lua
+++ client/function.lua
@@ -6,14 +6,14 @@ local inAdvaced, inNormal, outAdvanced, outNormal = inConfiguration.isAdvanced, 
 
 function inSide(isAdvanced)
     local config = isAdvanced and inAdvaced or inNormal
-    local success = exports[outExport]:skillbar(table.unpack(config))
+    local success = exports[inExport]:startLockpick(table.unpack(config))
     ---@param you_can_only_change THIS ↑↑↑↑↑↑↑↑
     return success
 end
 
 function outSide(isAdvanced)
     local config = isAdvanced and outAdvanced or outNormal
-    local success = exports[outExport]:skillbar(table.unpack(config))
+    local success = exports[outExport]:createGame(table.unpack(config))
     ---@param you_can_only_change THIS ↑↑↑↑↑↑↑↑
     return success
 end
